52f5dc343ab075faebf34ba01fc9c2610dd12ba6
OscarSah <oscar@cybertekschool.com>
2021-12-30 02:42:52 +0300

    Driver Class Chrome Options

--- src/test/java/com/zerobank/stepdefinitions/LoginStepDefs.java
+++ src/test/java/com/zerobank/stepdefinitions/LoginStepDefs.java
@@ -1,5 +1,6 @@
 package com.zerobank.stepdefinitions;
 
+import com.zerobank.pages.LoginPage;
 import com.zerobank.utilities.ConfigurationReader;
 import com.zerobank.utilities.Driver;
 import io.cucumber.java.en.Given;
@@ -7,14 +8,16 @@ import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
 
 public class LoginStepDefs {
+    LoginPage loginPage = new LoginPage();
+
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() {
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
         Driver.getDriver().findElement(By.id("signin_button")).click();
     }
     @Given("User logins with username {string} and password {string}")
-    public void user_logins_with_username_and_password(String string, String string2) {
-
+    public void user_logins_with_username_and_password(String u, String p) {
+        loginPage.login(u,p);
     }
     @Then("the {string} page should be displayed")
     public void the_page_should_be_displayed(String string) {
--- src/test/java/com/zerobank/utilities/Driver.java
+++ src/test/java/com/zerobank/utilities/Driver.java
@@ -3,7 +3,9 @@ package com.zerobank.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -50,6 +52,15 @@ public class Driver {
                     case "firefox":
                         WebDriverManager.firefoxdriver().setup();
                         driverPool.set(new FirefoxDriver());
+                        driverPool.get().manage().window().maximize();
+                        driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                        break;
+                    case "chromeSSL":
+                        WebDriverManager.chromedriver().setup();
+                        ChromeOptions capability = new ChromeOptions();
+                        capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS,true);
+                        capability.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS,true);
+                        driverPool.set(new ChromeDriver(capability));
                         driverPool.get().manage().window().maximize();
                         driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                         break;
