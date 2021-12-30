d53030fe36dc836516e88a491023adebcacd425d
OscarSah <oscar@cybertekschool.com>
2021-12-31 01:47:54 +0300

    Page Tab located

---
+++ src/test/java/com/zerobank/pages/BasePage.java
@@ -0,0 +1,18 @@
+package com.zerobank.pages;
+
+import com.zerobank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+    public BasePage (){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(className = "active")
+    public WebElement selectedTab;
+
+
+
+}
