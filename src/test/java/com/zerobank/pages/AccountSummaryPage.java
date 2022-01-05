a421749ae16fa45925c95a1bbbbb0427431d5342
OscarSah <oscar@cybertekschool.com>
2022-01-06 01:49:30 +0300

    account summary started

--- src/test/java/com/zerobank/pages/AccountSummaryPage.java
+++ src/test/java/com/zerobank/pages/AccountSummaryPage.java
@@ -1,4 +1,14 @@
 package com.zerobank.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class AccountSummaryPage extends BasePage {
+
+    @FindBy(tagName = "h2")
+    public List<WebElement> accounts;
+
+
 }
